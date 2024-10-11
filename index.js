e45734edbea50e0ec9c681ede221d17e748e9a07
werdycf <werdycloudflare@gmail.com>
2024-10-11 14:13:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'c120a034-de68-4fae-9392-634179841468';
 
-const proxyIPs = ['192.168.1.1'];
+const proxyIPs = ['104.168.28.65'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
